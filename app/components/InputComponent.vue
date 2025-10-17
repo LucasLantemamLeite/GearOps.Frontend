6f58f1c21f593bc78b224a18c1e477427248472d
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-16 23:15:46 -0300

    feat: cria o component de input de dados

---
+++ app/components/InputComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <input v-bind="$attrs" :class="class" :name="name" :id="id" :value="value" :placeholder="placeholder" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name?: string;
+  id?: string;
+  class?: string;
+  value?: any;
+  placeholder?: string;
+}>();
+</script>
