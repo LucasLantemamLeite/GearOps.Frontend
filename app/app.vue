df3f55014824dbf9695920567b123ee753c4a978
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-16 22:34:30 -0300

    feat: adiciona suporte à pages e configuração padrão de estilo

--- app/app.vue
+++ app/app.vue
@@ -1,1 +1,21 @@
-<template></template>
+<template>
+  <NuxtPage />
+</template>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  position: relative;
+  width: 100vw;
+  height: auto;
+}
+</style>
