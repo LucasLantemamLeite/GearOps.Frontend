9aa6ed615c41bc563add022defd7ded13fd074d6
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-16 23:17:24 -0300

    feat: adiciona o layout de controle e o delimitador de tamanho para separação da tela

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,20 @@
 <template>
-  <FooterLayout />
   <Title>DashBoard</Title>
+
+  <section class="index__delimiter">
+    <ControlLayout />
+  </section>
+
+  <FooterLayout />
 </template>
 
 <script setup lang="ts">
 import FooterLayout from "~/layouts/FooterLayout.vue";
+import ControlLayout from "~/sections/FilterLayout/ControlLayout.vue";
 </script>
+
+<style lang="scss">
+.index__delimiter {
+  width: 90%;
+}
+</style>
