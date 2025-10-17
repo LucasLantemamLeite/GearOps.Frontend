f57f4f5b0553c9b5f304da28d3cfa997ec240ad6
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-16 22:41:19 -0300

    chore: altera o nome da pasta para seguir case-sensitive

---
+++ app/components/ImageComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <img v-if="staticImg" :src="staticImg" :alt="altImg" :class="class" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  staticImg?: string;
+  altImg?: string;
+  class?: string;
+}>();
+</script>
