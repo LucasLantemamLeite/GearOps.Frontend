e2724daeb7bb5241c7d598e12e3baf5f3aee622e
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-19 15:30:05 -0300

    feat: adiciona a captura de erros e notificação na aplicação

--- app/services/Requests/GetDevicesService.ts
+++ app/services/Requests/GetDevicesService.ts
@@ -1,6 +1,7 @@
 import { Devices } from "~/global/Devices";
 import { apiRequestAsync } from "../helpers/ApiService";
 import { loadingHide, loadingShow } from "~/global/Loading";
+import { setNotification } from "~/global/Notification";
 
 export async function getDevicesService(take: number = 30, page: number = 1) {
   loadingShow();
@@ -8,8 +9,11 @@ export async function getDevicesService(take: number = 30, page: number = 1) {
   try {
     const result = await apiRequestAsync(`/device/${take}/${page}`, "GET", 15);
 
+    setNotification(`${result.message}`, 5, "Success");
+
     Devices.value = result.data;
-  } catch {
+  } catch (err: any) {
+    setNotification(String(err), 5, "Error");
   } finally {
     loadingHide();
   }
