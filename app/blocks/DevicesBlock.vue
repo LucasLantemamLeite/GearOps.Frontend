4ecb21c7594d4fc5c04e274139eefcfa8a37142a
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-18 23:26:10 -0300

    feat: cria o block para visualização de máquinas

---
+++ app/blocks/DevicesBlock.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <div v-for="device in GlobalDevices">
+      <p>{{ device.name }}</p>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { getDevicesService } from "~/services/requests/GetDevicesService";
+import { GlobalDevices } from "~/global/Devices";
+
+onMounted(async () => {
+  await getDevicesService();
+});
+</script>
