fc18e80bf94ac666ce91d59188434a161f093755
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-19 19:14:18 -0300

    feat: adiciona um pequeno efeito de divisão entre o nome e a imagem da máquina

--- app/blocks/DevicesBlock.vue
+++ app/blocks/DevicesBlock.vue
@@ -1,9 +1,11 @@
 <template>
-  <section v-if="Devices.length != 0" class="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] w-full h-auto items-center p-4 gap-4 mb-8 rounded-[0.9rem] border-2 border-[#272727] shadow-md md:mb-[7rem]">
-    <div v-for="device in Devices" class="flex relative h-[20rem] items-center justify-center p-4 flex-col bg-[#272727] text-[white] text-[1.5rem] rounded-[0.9rem] cursor-pointer shadow-md md:transition-all md:hover:translate-y-[3px]">
+  <section v-if="Devices.length != 0" class="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] w-full h-auto items-center p-4 gap-4 mb-8 rounded-[0.9rem] border-2 border-[#272727] shadow-md md:mb-[7rem]">
+    <div v-for="device in Devices" class="flex relative w-full h-[20rem] items-center justify-center p-4 flex-col bg-[#272727] text-[white] text-[1.5rem] rounded-[0.9rem] cursor-pointer shadow-md md:transition-all md:hover:translate-y-[3px]">
       <div :style="{ backgroundColor: getColorByStatus(device.status) }" class="flex absolute w-full h-[1rem] top-0 rounded-t-[0.5rem]"></div>
 
       <ImageComponent :static-img="`/Images/${getImageByType(device.type)}.png`" :alt-img="`${getImageByType(device.type)} Image`" class="w-[9rem] mb-10 md:w-[10rem]" />
+
+      <hr class="absolute bottom-[5.5rem] h-1 w-[85%] bg-[#92929250] border-none rounded-[0.9rem]" />
 
       <p class="absolute bottom-4 p-2 text-center text-[1.4rem] border-2 rounded-[0.9rem]">{{ device.name }}</p>
     </div>
