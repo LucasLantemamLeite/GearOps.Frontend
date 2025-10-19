4cfe1bba970e1e9039720a52a0ef2d278e1ed2dc
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-18 22:34:58 -0300

    feat: adiciona pacote e configuração do tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
   ssr: false,
+
+  css: ["~/assets/css/tailwind.css"],
+  modules: ["@nuxtjs/tailwindcss"],
 });
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./app/blocks/**/*.{vue,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
