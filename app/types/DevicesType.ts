653232cce149813c71de2797f03cc9a4163178be
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-18 23:24:15 -0300

    feat: adiciona o type representando os dispositivos do servidor

---
+++ app/types/DevicesType.ts
@@ -0,0 +1,11 @@
+export interface DeviceType {
+  id: string;
+  name: string;
+  type: number;
+  status: number;
+  createdAt: Date;
+  updatedAt: Date;
+  start?: Date;
+  return?: Date;
+  active: Boolean;
+}
