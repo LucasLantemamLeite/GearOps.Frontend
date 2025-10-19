01240034acf14f1f904fe20d52f8416efc23412e
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-19 15:36:25 -0300

    Recupera GetDevicesService.ts apagado

---
+++ app/services/requests/GetDevicesService.ts
@@ -0,0 +1,20 @@
+import { Devices } from "~/global/Devices";
+import { apiRequestAsync } from "../helpers/ApiService";
+import { loadingHide, loadingShow } from "~/global/Loading";
+import { setNotification } from "~/global/Notification";
+
+export async function getDevicesService(take: number = 30, page: number = 1) {
+  loadingShow();
+
+  try {
+    const result = await apiRequestAsync(`/device/${take}/${page}`, "GET", 15);
+
+    setNotification(`${result.message}`, 5, "Success");
+
+    Devices.value = result.data;
+  } catch (err: any) {
+    setNotification(String(err), 5, "Error");
+  } finally {
+    loadingHide();
+  }
+}
