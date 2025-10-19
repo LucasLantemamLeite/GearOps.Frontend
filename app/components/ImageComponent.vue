3d12e24c1170b625655cd74873e1dd8240b9ec92
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-19 15:38:13 -0300

    Recupera ImageComponent.vue apagado

---
+++ app/components/ImageComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <img v-if="staticImg" :src="staticImg" :alt="altImg" v-bind="$attrs" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  staticImg?: string;
+  altImg?: string;
+}>();
+</script>
