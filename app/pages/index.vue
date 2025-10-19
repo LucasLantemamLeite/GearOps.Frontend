9b48462319d9b44450d4e2f231e6bf896c6e1526
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-18 23:27:05 -0300

    feat: adiciona o block de dispositivos na raiz da aplicação

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <Title>DashBoard</Title>
 
-  <section class="w-[90%]">
+  <section class="w-[95%]">
     <ControlBlock />
+    <DevicesBlock />
   </section>
 
   <MediaLayout />
@@ -10,5 +11,6 @@
 
 <script setup lang="ts">
 import ControlBlock from "~/blocks/ControlBlock.vue";
+import DevicesBlock from "~/blocks/DevicesBlock.vue";
 import MediaLayout from "~/layouts/MediaLayout.vue";
 </script>
