c3436fa47f81bef1db05c2c0dd1002108a8daebf
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-19 14:28:58 -0300

    feat: adiciona a chamada do componente de loading

--- app/services/Requests/GetDevicesService.ts
+++ app/services/Requests/GetDevicesService.ts
@@ -1,10 +1,16 @@
 import { Devices } from "~/global/Devices";
 import { apiRequestAsync } from "../helpers/ApiService";
+import { loadingHide, loadingShow } from "~/global/Loading";
 
 export async function getDevicesService(take: number = 30, page: number = 1) {
+  loadingShow();
+
   try {
     const result = await apiRequestAsync(`/device/${take}/${page}`, "GET", 15);
 
     Devices.value = result.data;
-  } catch {}
+  } catch {
+  } finally {
+    loadingHide();
+  }
 }
