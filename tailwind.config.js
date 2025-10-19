4c8934ad61eb31469119fe3242a8b487c6ba064f
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-19 14:29:17 -0300

    feat: adiciona o caminho dos providers para estilo com tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./app/blocks/**/*.{vue,js,ts}"],
+  content: ["./app/blocks/**/*.{vue,js,ts}", "./app/providers/**/*.{vue,js,ts}"],
   theme: {
     extend: {},
   },
