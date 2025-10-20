9c9b1df4e3196ff80430a5428cc7b6d312e9512e
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-20 18:35:58 -0300

    feat: pequeno melhoria no provider de notificações com ênfase na responsividade

--- app/providers/NotificationProvider.vue
+++ app/providers/NotificationProvider.vue
@@ -1,10 +1,13 @@
 <template>
-  <div v-if="notification.message != ''" class="flex fixed max-w-[30rem] h-auto justify-center items-center px-8 py-4 gap-4 bg-[#272727] bottom-[1rem] right-[1rem] rounded-[0.9rem] pointer-events-none shadow-md md:bottom-[6.5rem] md:max-w-[40rem] md:px-6 md:py-4">
+  <div
+    v-if="notification.message != ''"
+    class="flex fixed max-w-[25rem] h-auto max-h-[10rem] overflow-auto justify-center items-center px-8 py-4 gap-4 bg-[#272727] bottom-[1rem] right-[1rem] rounded-[0.9rem] pointer-events-none shadow-md md:bottom-[6.5rem] md:gap-8 md:max-w-[35rem] md:px-8 md:py-4 lg:max-w-[40rem]"
+  >
     <div :style="{ backgroundColor: getColorByType(notification.type) }" class="absolute w-[1rem] h-full left-0 rounded-l-[0.5rem]"></div>
 
-    <ImageComponent :static-img="`/Icons/${notification.type}Icon.svg`" :alt-img="`${notification.type} Icon`" class="w-[3rem] md:w-[4rem]" />
+    <ImageComponent :static-img="`/Icons/${notification.type}Icon.svg`" :alt-img="`${notification.type} Icon`" class="w-[5rem] md:w-[6rem] lg:w-[7rem]" />
 
-    <p class="text-white text-center text-[1.2rem] md:text-[1.4rem]">{{ notification.message }}</p>
+    <p class="text-white text-[1.2rem] md:text-[1.6rem]">{{ notification.message }}</p>
   </div>
 </template>
 
