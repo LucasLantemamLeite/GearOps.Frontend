183876f1131f4cf78d2b63ece3074d6bd74ecdeb
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-20 23:31:59 -0300

    feat: cria o objeto de device type e status para ser usado com componente de select

---
+++ app/types/DeviceType.ts
@@ -0,0 +1,7 @@
+export const DeviceType: Record<string, { label: string; value: number }> = {
+  Forklift: { label: "Empilhadeira", value: 1 },
+  Excavator: { label: "Excavadeira", value: 2 },
+  Crane: { label: "Guindaste", value: 3 },
+  Bulldozer: { label: "Trator de Esteira", value: 4 },
+  Loader: { label: "Carregadeira", value: 5 },
+};
