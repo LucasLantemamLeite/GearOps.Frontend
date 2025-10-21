c651d7eaaaf87b68e7d1fda7c59c00a548e2140f
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-20 23:35:43 -0300

    feat: adiciona o componente de select

---
+++ app/components/SelectComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <select @change="updateModelValue" :value="modelValue" :name="name" :id="id">
+    <option v-for="(obj, key) in type" :key="key" :value="obj.value">{{ obj.label }}</option>
+  </select>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name?: string;
+  id?: string;
+  modelValue?: number;
+  type: Record<string, { label: string; value: number }>;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: number): void;
+}>();
+
+function updateModelValue(e: Event) {
+  const target = e.target as HTMLSelectElement;
+
+  emit("update:modelValue", Number(target.value));
+}
+</script>
