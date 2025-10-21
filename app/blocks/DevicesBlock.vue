544320ba0f1f6313c179dfa5140362775e997cbe
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-20 23:37:06 -0300

    feat: adiciona a lógica para editar uma máquina existente

--- app/blocks/DevicesBlock.vue
+++ app/blocks/DevicesBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <section v-if="Devices.length != 0" class="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] w-full h-auto items-center p-4 gap-4 mb-8 rounded-[0.9rem] border-2 border-[#272727] shadow-md md:mb-[7rem]">
-    <div v-for="device in Devices" class="flex relative w-full h-[20rem] items-center justify-center p-4 flex-col bg-[#272727] text-[white] text-[1.5rem] rounded-[0.9rem] cursor-pointer shadow-md md:transition-all md:hover:translate-y-[3px]">
+    <div v-for="device in Devices" @click="openEdit(device)" class="flex relative w-full h-[20rem] items-center justify-center p-4 flex-col bg-[#272727] text-[white] text-[1.5rem] rounded-[0.9rem] cursor-pointer shadow-md md:transition-all md:hover:translate-y-[3px]">
       <div :style="{ backgroundColor: getColorByStatus(device.status) }" class="flex absolute w-full h-[1rem] top-0 rounded-t-[0.5rem]"></div>
 
       <ImageComponent :static-img="`/Images/${getImageByType(device.type)}.png`" :alt-img="`${getImageByType(device.type)} Image`" class="w-[9rem] mb-10 md:w-[10rem]" />
@@ -10,6 +10,8 @@
       <p class="absolute bottom-4 p-2 text-center text-[1.4rem] border-2 rounded-[0.9rem]">{{ device.name }}</p>
     </div>
   </section>
+
+  <DeviceModelBlock :device="deviceEdit!" v-model="isEditing" />
 </template>
 
 <script setup lang="ts">
@@ -20,6 +22,11 @@ import { createdConnect } from "~/services/signalR/CreatedConnect";
 import { deletedConnect } from "~/services/signalR/DeletedConnect";
 import { updatedConnect } from "~/services/signalR/UpdatedConnect";
 import { setNotification } from "~/global/Notification";
+import type { Device } from "~/types/Device";
+import DeviceModelBlock from "./DeviceModelBlock.vue";
+
+const isEditing = ref(false);
+const deviceEdit = ref<Device>();
 
 onMounted(async () => {
   await getDevicesService();
@@ -38,41 +45,8 @@ onMounted(async () => {
   }
 });
 
-function getImageByType(type: number): string {
-  switch (type) {
-    case 1:
-      return "Forklift";
-
-    case 2:
-      return "Excavator";
-
-    case 3:
-      return "Crane";
-
-    case 4:
-      return "Bulldozer";
-
-    case 5:
-      return "Loader";
-
-    default:
-      return "Forklift";
-  }
-}
-
-function getColorByStatus(status: number): string {
-  switch (status) {
-    case 1:
-      return "#32D74A";
-
-    case 2:
-      return "#D73732";
-
-    case 3:
-      return "#D7D731";
-
-    default:
-      return "#32D74A";
-  }
+function openEdit(device: Device) {
+  deviceEdit.value = device; // passa a máquina clicada
+  isEditing.value = true; // abre o modal
 }
 </script>
