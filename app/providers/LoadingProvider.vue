74648e29f6831b92db823512b65961f0fd20f643
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-20 23:34:40 -0300

    feat: coloco o componente de loading na frente de tudo na tela

--- app/providers/LoadingProvider.vue
+++ app/providers/LoadingProvider.vue
@@ -1,5 +1,5 @@
 <template>
-  <section v-if="Loading" class="flex justify-center items-center fixed w-full h-full inset-0 z-10 bg-[rgba(0,0,0,0.6)] backdrop-blur-[3px]">
+  <section v-if="Loading" class="flex justify-center items-center fixed w-full h-full inset-0 z-50 bg-[rgba(0,0,0,0.6)] backdrop-blur-[3px]">
     <ImageComponent static-img="/Icons/LoadingIcon.svg" alt-img="Loading Icon" class="w-[5rem] animate-spin md:w-[10rem] lg:w-[15rem]" />
   </section>
 </template>
