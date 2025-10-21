6ade82ad2996a7d3b57e32f917af2a0c9f0cd2dc
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-10-20 23:36:01 -0300

    feat: adiciona novas funcionalidades para o input component

--- app/components/InputComponent.vue
+++ app/components/InputComponent.vue
@@ -1,12 +1,26 @@
 <template>
-  <input v-bind="$attrs" :name="name" :id="id" :value="value" :placeholder="placeholder" />
+  <input @input="updateModelValue" v-bind="$attrs" :type="type" :name="name" :id="id" :value="modelValue ?? ''" :placeholder="placeholder" />
 </template>
 
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   name?: string;
   id?: string;
-  value?: any;
+  modelValue?: string | number;
   placeholder?: string;
+  type?: string;
 }>();
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string | number): void;
+}>();
+
+function updateModelValue(e: Event) {
+  var target = e.target as HTMLInputElement;
+  let value: string | number = target.value;
+
+  if (props.type === "number") value = Number(value);
+
+  emit("update:modelValue", value);
+}
 </script>
