1cf2b0bd51c2f57bfe0f35aa0bd2ffa4e29b4aad
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-22 14:58:54 -0300

    feat: sempre que uma máquina for deletada permantemente, será realizado uma nova requisição para o servidor garantindo que a lista de máquinas sempre esteja no máximo possível

--- app/services/signalR/DeletedConnect.ts
+++ app/services/signalR/DeletedConnect.ts
@@ -1,10 +1,12 @@
 import type { HubConnection } from "@microsoft/signalr";
 import { Devices, orderByType } from "~/global/Devices";
 import type { Device } from "~/types/Device";
+import { getDevicesService } from "../requests/GetDevicesService";
 
-export function deletedConnect(connection: HubConnection) {
+export function deletedConnect(connection: HubConnection, take: number = 30, page: number = 1) {
   connection.on("DeviceDeleted", (device: Device) => {
     Devices.value = [...Devices.value.filter((x) => x.id != device.id)];
+    getDevicesService(take, page);
     orderByType();
   });
 }
