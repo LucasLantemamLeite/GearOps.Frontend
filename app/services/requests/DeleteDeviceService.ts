1e83b10e8d0a2b4f4ce657b2630663e1ee68a779
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-22 22:53:30 -0300

    feat: cria a função de requisição para exclusão de dispositivos cadastrados

---
+++ app/services/requests/DeleteDeviceService.ts
@@ -0,0 +1,25 @@
+import { loadingHide, loadingShow } from "~/global/Loading";
+import { setNotification } from "~/global/Notification";
+import { apiRequestAsync } from "../helpers/ApiService";
+
+export async function deleteDeviceService(id: string, closeModal: () => void) {
+  if (!id) setNotification("Falha ao capturar o id do dispositivo.", 7, "Info");
+
+  console.log(id);
+
+  loadingShow();
+
+  const dto = { id: id };
+
+  try {
+    const result = await apiRequestAsync("/device", "DELETE", 30, dto);
+
+    setNotification(`${result.message}`, 7, "Success");
+
+    closeModal();
+  } catch (err: unknown) {
+    setNotification(String(err), 7, "Error");
+  } finally {
+    loadingHide();
+  }
+}
