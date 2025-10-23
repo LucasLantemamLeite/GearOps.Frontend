38a74bf3f74451d23c887fe84b27a076ebd92996
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-23 17:36:21 -0300

    feat: permite receber null nas propriedades de start e return

--- app/types/Device.ts
+++ app/types/Device.ts
@@ -5,7 +5,7 @@ export interface Device {
   status: number;
   createdAt: Date;
   updatedAt: Date;
-  start?: Date | string;
-  return?: Date | string;
+  start?: Date | string | null;
+  return?: Date | string | null;
   active: Boolean;
 }
