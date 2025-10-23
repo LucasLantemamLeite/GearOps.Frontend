97a1a5f8fa6ee6e78a1c99313c91da8394314ef2
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-22 22:47:40 -0300

    refactor: remove a chamada auto do servidor, será criado uma nova rota espeficamente para isso

--- app/services/signalR/DeletedConnect.ts
+++ app/services/signalR/DeletedConnect.ts
@@ -1,12 +1,10 @@
 import type { HubConnection } from "@microsoft/signalr";
 import { Devices, orderByType } from "~/global/Devices";
 import type { Device } from "~/types/Device";
-import { getDevicesService } from "../requests/GetDevicesService";
 
-export function deletedConnect(connection: HubConnection, take: number = 30, page: number = 1) {
+export function deletedConnect(connection: HubConnection) {
   connection.on("DeviceDeleted", (device: Device) => {
     Devices.value = [...Devices.value.filter((x) => x.id != device.id)];
-    getDevicesService(take, page);
     orderByType();
   });
 }
