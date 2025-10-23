17ec4e3db4507760a9ef5ebf7dd0daf2df37f02a
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-23 10:38:31 -0300

    feat: importa as funções de shared e alterna o record para um objeto mais descritivo

--- app/pages/index/fragments/DevicePreview.vue
+++ app/pages/index/fragments/DevicePreview.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex relative w-full h-[20rem] flex-col items-center justify-center p-4 mb-2 bg-[#272727] text-[white] text-[1.5rem] rounded-[0.9rem] shadow-md">
-    <div :style="{ backgroundColor: getColorByStatus(previewDevice!.status) }" class="flex absolute w-full h-[1rem] top-0 rounded-t-[0.5rem]"></div>
+    <div :style="{ backgroundColor: getColorByStatus(previewDevice.status) }" class="flex absolute w-full h-[1rem] top-0 rounded-t-[0.5rem]"></div>
 
-    <ImageComponent :static-img="`/Images/${getImageByType(previewDevice!.type)}.png`" :alt-img="`${getImageByType(previewDevice!.type)} Image`" class="w-[9rem] mb-10 md:w-[10rem]" />
+    <ImageComponent :static-img="`/Images/${getImageByType(previewDevice.type)}.png`" :alt-img="`${getImageByType(previewDevice!.type)} Image`" class="w-[9rem] mb-10 md:w-[10rem]" />
 
     <hr class="absolute bottom-[5.5rem] h-1 w-[85%] bg-[#92929250] border-none rounded-[0.9rem]" />
 
@@ -11,7 +11,9 @@
 </template>
 
 <script setup lang="ts">
+import { getColorByStatus, getImageByType } from "~/shared/Devices";
+
 defineProps<{
-  previewDevice: Record<string, any>;
+  previewDevice: { name: string; type: number; status: number };
 }>();
 </script>
