3eab3b0c0169faca0f5ff74d814d3ae0d0291ee0
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-23 17:44:41 -0300

    feat: adiciona uma pequena valição antes de enviar o dto com error ou avisos de datas de manutenção

--- app/services/requests/createDevice.ts
+++ app/services/requests/createDevice.ts
@@ -5,8 +5,6 @@ import { setNotification } from "~/shared/Notification";
 export async function createDevice(e: Event, closeModal: () => void) {
   e.preventDefault();
 
-  loadingShow();
-
   const target = e.target as HTMLElement;
   const form = target.closest("form") as HTMLFormElement;
 
@@ -21,10 +19,28 @@ export async function createDevice(e: Event, closeModal: () => void) {
     return: payload.return ? new Date(payload.return + ":00").toISOString() : null,
   };
 
+  if (!payload.name) {
+    setNotification("O nome do dispositivo é obrigatório.", 7, "Error");
+    return;
+  }
+
+  if (dto.start && dto.return) {
+    if (dto.start > dto.return) {
+      setNotification("Data de retorno da manutenção não pode ser menor do que a data de inicio.", 7, "Warning");
+      return;
+    }
+  }
+
+  loadingShow();
+
   try {
     const result = await apiRequest("/device", "POST", 30, dto);
 
-    setNotification(`${result.message}`, 7, "Success");
+    if (dto.start && !dto.return) {
+      setNotification(`${result.message} Porém só foi definido data de inicio da manutenção.`, 10, "Info");
+    } else if (!dto.start && dto.return) {
+      setNotification(`${result.message} Porém só foi definido a data de retorno da manutenção.`, 10, "Info");
+    } else setNotification(`${result.message}`, 7, "Success");
 
     closeModal();
   } catch (err: unknown) {
--- app/services/requests/updateDevice.ts
+++ app/services/requests/updateDevice.ts
@@ -7,8 +7,6 @@ export async function updateDevice(e: Event, id: string, closeModel: () => void)
 
   e.preventDefault();
 
-  loadingShow();
-
   const target = e.target as HTMLElement;
   const form = target.closest("form") as HTMLFormElement;
 
@@ -24,10 +22,28 @@ export async function updateDevice(e: Event, id: string, closeModel: () => void)
     return: payload.return ? new Date(payload.return + ":00").toISOString() : null,
   };
 
+  if (!payload.name) {
+    setNotification("O nome do dispositivo é obrigatório.", 7, "Error");
+    return;
+  }
+
+  if (dto.start && dto.return) {
+    if (dto.start > dto.return) {
+      setNotification("Data de retorno da manutenção não pode ser menor do que a data de inicio.", 7, "Warning");
+      return;
+    }
+  }
+
+  loadingShow();
+
   try {
     const result = await apiRequest("/device", "PUT", 30, dto);
 
-    setNotification(`${result.message}`, 7, "Success");
+    if (dto.start && !dto.return) {
+      setNotification(`${result.message} Porém só foi definido data de inicio da manutenção.`, 10, "Info");
+    } else if (!dto.start && dto.return) {
+      setNotification(`${result.message} Porém só foi definido a data de retorno da manutenção.`, 10, "Info");
+    } else setNotification(`${result.message}`, 7, "Success");
 
     closeModel();
   } catch (err: unknown) {
