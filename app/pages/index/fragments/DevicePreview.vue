ff73dc6d0ea0218d42917c4cd5cc4cb5277ef1c6
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-23 17:34:43 -0300

    feat: adiciona as informações de incio e retorno de manutenção tanto na lista de máquinas quanto na pré-visualização

--- app/pages/index/fragments/DevicePreview.vue
+++ app/pages/index/fragments/DevicePreview.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="flex relative w-[80%] h-[20rem] flex-col items-center justify-center p-4 mb-2 bg-[#272727] text-[white] text-[1.5rem] rounded-[0.9rem] shadow-md">
+  <div class="flex relative w-[80%] h-[20rem] flex-col items-center p-4 mb-2 bg-[#272727] text-[white] text-[1.5rem] rounded-[0.9rem] shadow-md md:h-[24rem]">
     <div :style="{ backgroundColor: getColorByStatus(previewDevice.status) }" class="flex absolute w-full h-[1rem] top-0 rounded-t-[0.5rem]"></div>
 
-    <ImageComponent :static-img="`/Images/${getImageByType(previewDevice.type)}.png`" :alt-img="`${getImageByType(previewDevice!.type)} Image`" class="w-[9rem] mb-10 md:w-[10rem]" />
+    <p class="p-2 mt-4 mb-2 text-center text-[1.4rem] border-2 rounded-[0.9rem] md:text-[1.6rem]">{{ previewDevice.name || "Nome da máquina" }}</p>
 
-    <hr class="absolute bottom-[5.5rem] h-1 w-[85%] bg-[#92929250] border-none rounded-[0.9rem]" />
+    <ImageComponent :static-img="`/Images/${getImageByType(previewDevice.type)}.png`" :alt-img="`${getImageByType(previewDevice!.type)} Image`" class="mb-2 w-[8rem] mb-10 md:w-[10rem]" />
 
-    <p class="absolute bottom-4 p-2 text-center text-[1.4rem] border-2 rounded-[0.9rem]">{{ previewDevice.name == "" ? "Nome da máquina" : previewDevice.name }}</p>
+    <div v-if="previewDevice.status === 3" class="flex w-[70%] items-center flex-col gap-2">
+      <div class="flex items-center w-full justify-between gap-2">
+        <ImageComponent static-img="/Icons/StartIcon.svg" class="w-[2rem]" />
+        <p class="text-white text-[1.2rem] md:text-[1.6rem]">{{ previewDevice.start ? formateDateISO(previewDevice.start, true) : "Não definido" }}</p>
+      </div>
+
+      <hr class="h-[0.2rem] w-[120%] bg-[#92929250] border-none rounded-[0.9rem]" />
+
+      <div class="flex items-center w-full justify-between gap-2">
+        <ImageComponent static-img="/Icons/ReturnIcon.svg" class="w-[2rem]" />
+        <p class="text-white text-[1.2rem] md:text-[1.6rem]">{{ previewDevice.return ? formateDateISO(previewDevice.return, true) : "Não definido" }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { formateDateISO } from "~/utils/dates/formateDateISO";
 import { getColorByStatus, getImageByType } from "~/shared/Devices";
 
 defineProps<{
-  previewDevice: { name: string; type: number; status: number };
+  previewDevice: { name: string; type: number; status: number; start?: string | Date | null; return?: string | Date | null };
 }>();
 </script>
--- app/pages/index/fragments/ViewDevices.vue
+++ app/pages/index/fragments/ViewDevices.vue
@@ -1,13 +1,25 @@
 <template>
-  <section v-if="Devices.length != 0" class="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] w-full h-auto items-center p-4 gap-4 mb-8 rounded-[0.9rem] border-2 border-[#272727] shadow-md md:mb-[7rem]">
-    <div v-for="device in Devices" @click="openEdit(device)" class="flex relative w-full h-[20rem] items-center justify-center p-4 flex-col bg-[#272727] text-[white] text-[1.5rem] rounded-[0.9rem] lg:cursor-pointer shadow-md md:transition-all md:hover:translate-y-[3px]">
+  <section v-if="Devices.length != 0" class="grid grid-cols-[repeat(auto-fill,minmax(220px,1fr))] w-full h-auto items-center p-4 gap-4 mb-8 rounded-[0.9rem] border-2 border-[#272727] shadow-md md:grid-cols-[repeat(auto-fill,minmax(250px,1fr))] md:mb-[7rem]">
+    <div v-for="device in Devices" @click="openEdit(device)" class="flex relative w-full h-[22rem] items-center gap-4 flex-col bg-[#272727] text-[white] text-[1.5rem] rounded-[0.9rem] md:h-[25rem] shadow-md lg:cursor-pointer lg:transition-all lg:hover:translate-y-[3px]">
       <div :style="{ backgroundColor: getColorByStatus(device.status) }" class="flex absolute w-full h-[1rem] top-0 rounded-t-[0.5rem]"></div>
 
-      <ImageComponent :static-img="`/Images/${getImageByType(device.type)}.png`" :alt-img="`${getImageByType(device.type)} Image`" class="w-[9rem] mb-10 md:w-[10rem]" />
+      <p class="p-2 mt-8 mb-2 text-center text-[1.4rem] border-2 rounded-[0.9rem] md:text-[1.6rem]">{{ device.name }}</p>
 
-      <hr class="absolute bottom-[5.5rem] h-1 w-[85%] bg-[#92929250] border-none rounded-[0.9rem]" />
+      <ImageComponent :static-img="`/Images/${getImageByType(device.type)}.png`" :alt-img="`${getImageByType(device.type)} Image`" class="mb-2 w-[8rem] md:w-[10rem]" />
 
-      <p class="absolute bottom-4 p-2 text-center text-[1.4rem] border-2 rounded-[0.9rem]">{{ device.name }}</p>
+      <div v-if="device.status === 3" class="flex w-[70%] items-center flex-col gap-2">
+        <div class="flex items-center w-full justify-between gap-2">
+          <ImageComponent static-img="/Icons/StartIcon.svg" class="w-[2rem]" />
+          <p class="text-white text-[1.2rem] md:text-[1.6rem]">{{ device.start ? formateDateISO(device.start, true) : "Não definido" }}</p>
+        </div>
+
+        <hr class="h-[0.2rem] w-[120%] bg-[#92929250] border-none rounded-[0.9rem]" />
+
+        <div class="flex items-center w-full justify-between gap-2">
+          <ImageComponent static-img="/Icons/ReturnIcon.svg" class="w-[2rem]" />
+          <p class="text-white text-[1.2rem] md:text-[1.6rem]">{{ device.return ? formateDateISO(device.return, true) : "Não definido" }}</p>
+        </div>
+      </div>
     </div>
   </section>
 
@@ -23,6 +35,7 @@ import { createdConnect } from "~/services/signalR/events/createdConnect";
 import { updatedConnect } from "~/services/signalR/events/updatedConnect";
 import { deletedConnect } from "~/services/signalR/events/deletedConnect";
 import DeviceModal from "./DeviceModal.vue";
+import { formateDateISO } from "~/utils/dates/formateDateISO";
 
 onMounted(async () => {
   getDevices();
