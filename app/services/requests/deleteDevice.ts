cfc957a77b3927f3f2568c645fe184e2af12871b
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-26 19:17:52 -0300

    fix: remove o consolo.log desnecessário e ajusta a rota da api

--- app/services/requests/deleteDevice.ts
+++ app/services/requests/deleteDevice.ts
@@ -1,18 +1,17 @@
 import { loadingHide, loadingShow } from "~/shared/Loading";
 import { setNotification } from "~/shared/Notification";
 import { apiRequest } from "./api";
+import { Pages } from "~/shared/Devices";
 
 export async function deleteDevice(id: string, closeModal: () => void) {
   if (!id) setNotification("Falha ao capturar o id do dispositivo.", 7, "Info");
-
-  console.log(id);
 
   loadingShow();
 
   const dto = { id: id };
 
   try {
-    const result = await apiRequest("/device", "DELETE", 30, dto);
+    const result = await apiRequest(`/device/${Pages.Take}`, "DELETE", 30, dto);
 
     setNotification(`${result.message}`, 7, "Success");
 
