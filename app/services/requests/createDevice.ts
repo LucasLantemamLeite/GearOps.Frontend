3ca80b6e3b90a4aab2c5e31f77a08e3539a587c3
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-26 19:17:20 -0300

    fix: ajusta o rota da api e diminui a frequência de notificação para não poluir visualmente

--- app/services/requests/createDevice.ts
+++ app/services/requests/createDevice.ts
@@ -1,6 +1,7 @@
 import { loadingHide, loadingShow } from "~/shared/Loading";
 import { apiRequest } from "./api";
 import { setNotification } from "~/shared/Notification";
+import { Pages } from "~/shared/Devices";
 
 export async function createDevice(e: Event, closeModal: () => void) {
   e.preventDefault();
@@ -26,7 +27,7 @@ export async function createDevice(e: Event, closeModal: () => void) {
 
   if (dto.start && dto.return) {
     if (dto.start > dto.return) {
-      setNotification("Data de retorno da manutenção não pode ser menor do que a data de inicio.", 7, "Warning");
+      setNotification("Data de retorno da manutenção não pode ser menor do que a data de inicio.", 7, "Attention");
       return;
     }
   }
@@ -34,7 +35,7 @@ export async function createDevice(e: Event, closeModal: () => void) {
   loadingShow();
 
   try {
-    const result = await apiRequest("/device", "POST", 30, dto);
+    const result = await apiRequest(`/device/${Pages.Take}`, "POST", 30, dto);
 
     if (dto.start && !dto.return) {
       setNotification(`${result.message} Porém só foi definido data de inicio da manutenção.`, 10, "Info");
