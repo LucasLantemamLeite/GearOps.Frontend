d260308640bac7c33eb45145ceee65bf0b479ddd
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-25 21:50:22 -0300

    refactor: remove completamente o módulo de tailwind da aplicação

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,4 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
   ssr: false,
-
-  css: ["~/assets/css/tailwind.css"],
-  modules: ["@nuxtjs/tailwindcss"],
 });
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./app/blocks/**/*.{vue,js,ts}", "./app/providers/**/*.{vue,js,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
