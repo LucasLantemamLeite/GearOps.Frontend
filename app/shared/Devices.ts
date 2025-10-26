09ae47afd27f3ccdc52eca8e11aa7fbb7a693f5e
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-26 19:21:28 -0300

    feat: adiciona a página atual, e quantos dispositivos será pego de acordo com o tamanho da tela do usuário

--- app/shared/Devices.ts
+++ app/shared/Devices.ts
@@ -4,8 +4,21 @@ export const Devices = ref<Device[]>([]);
 
 export const Pages = reactive({
   Count: 0,
-  Current: 0,
+  Current: 1,
+  Take: setTakeByScreenSize(),
 });
+
+function setTakeByScreenSize() {
+  const width = window.innerWidth;
+
+  if (width <= 600) {
+    return 15;
+  } else if (width <= 1024) {
+    return 20;
+  } else {
+    return 30;
+  }
+}
 
 export function getColorByStatus(status: number): string {
   switch (status) {
