bda68b615e43eb84f40945a006724bbd4d413d38
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-10-28 11:52:02 -0300

    feat: melhoria de lógica ao receber máquinas via signalR enquanto filtra

--- app/services/signalR/events/createdConnect.ts
+++ app/services/signalR/events/createdConnect.ts
@@ -4,6 +4,8 @@ import { connection } from "../connectionHub";
 
 export function createdConnect() {
   connection.on("DeviceCreated", (device: Device) => {
-    if (Filter.value && device.name.includes(Filter.value)) Devices.value = [...Devices.value, device];
+    if (Filter.value !== "" && device.name.toLowerCase().includes(Filter.value.toLowerCase())) {
+      Devices.value = [...Devices.value, device];
+    } else if (Filter.value === "") Devices.value = [...Devices.value, device];
   });
 }
